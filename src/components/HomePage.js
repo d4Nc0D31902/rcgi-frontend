d426e1355f8b71febc93d7c03f87cbac0e8278a2
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-04-17 09:06:19 +0800

    Footer / HomePage Redesign Skeleton

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography, Button } from "@mui/material";
+import { Grid, Typography, Button, Skeleton } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
@@ -12,7 +12,12 @@ const HomePage = () => {
       style={{ minHeight: "100vh" }}
     >
       <Grid item xs={12}>
-        <Typography variant="h2" align="center" gutterBottom>
+        <Typography
+          variant="h2"
+          align="center"
+          gutterBottom
+          style={{ marginTop: "20px" }}
+        >
           Welcome to Our Website
         </Typography>
       </Grid>
@@ -21,6 +26,43 @@ const HomePage = () => {
           We are delighted to have you here. Explore our website to discover
           exciting features and offerings.
         </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} align="center">
+        <Skeleton variant="rectangular" width="100%" height={300} />
+      </Grid>
+      <Grid item xs={12} sm={6} align="center">
+        <Skeleton variant="rectangular" width="100%" height={300} />
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button variant="contained" component={Link} to="/explore">
+          Explore More
+        </Button>
+      </Grid>
+      {/* Additional Placeholder Sections */}
+      <Grid item xs={12}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Our Services
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={4} align="center">
+        <Skeleton variant="rectangular" width="80%" height={200} />
+      </Grid>
+      <Grid item xs={12} sm={4} align="center">
+        <Skeleton variant="rectangular" width="80%" height={200} />
+      </Grid>
+      <Grid item xs={12} sm={4} align="center">
+        <Skeleton variant="rectangular" width="80%" height={200} />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Testimonials
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} align="center">
+        <Skeleton variant="rectangular" width="100%" height={100} />
+      </Grid>
+      <Grid item xs={12} sm={6} align="center">
+        <Skeleton variant="rectangular" width="100%" height={100} />
       </Grid>
     </Grid>
   );
