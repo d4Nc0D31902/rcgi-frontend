d6a2ae009abe6db75038697772075ffd68b26ae5
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-04-17 15:46:24 +0800

    Update User

--- src/components/admin/UpdateUser.js
+++ src/components/admin/UpdateUser.js
@@ -1,236 +1,162 @@
-import React, { Fragment, useState, useEffect } from 'react'
-
+import React, { Fragment, useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-
-
-
-import MetaData from '../layout/MetaData'
-
-import Sidebar from './Sidebar'
-
-import { toast } from 'react-toastify';
-
-import 'react-toastify/dist/ReactToastify.css';
-
-
-
-import { useDispatch, useSelector } from 'react-redux'
-
-import { updateUser, getUserDetails, clearErrors } from '../../actions/userActions'
-
-import { UPDATE_USER_RESET } from '../../constants/userConstants'
-
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateUser,
+  getUserDetails,
+  clearErrors,
+} from "../../actions/userActions";
+import { UPDATE_USER_RESET } from "../../constants/userConstants";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MetaData from "../layout/MetaData";
+import Sidebar from "./Sidebar";
+import {
+  TextField,
+  Button,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Grid,
+  Paper,
+} from "@mui/material";
 
 const UpdateUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [company, setCompany] = useState("");
+  const [employeeId, setEmployeeId] = useState("");
 
-    const [name, setName] = useState('')
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+  const { error, isUpdated } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.userDetails);
+  const { id } = useParams();
 
-    const [email, setEmail] = useState('')
-
-    const [role, setRole] = useState('')
-
-    const dispatch = useDispatch();
-
-    let navigate = useNavigate();
-
-    const { error, isUpdated } = useSelector(state => state.user);
-
-    const { user } = useSelector(state => state.userDetails)
-
-    const {id} = useParams();
-
-    const errMsg = (message = '') => toast.error(message, {
-
-        position: toast.POSITION.BOTTOM_CENTER
-
+  const errMsg = (message = "") =>
+    toast.error(message, {
+      position: toast.POSITION.BOTTOM_CENTER,
     });
 
-    const successMsg = (message = '') => toast.success(message, {
-
-        position: toast.POSITION.BOTTOM_CENTER
-
+  const successMsg = (message = "") =>
+    toast.success(message, {
+      position: toast.POSITION.BOTTOM_CENTER,
     });
 
-
-
-    useEffect(() => {
-
-        // console.log(user && user._id !== userId);
-
-        if (user && user._id !== id) {
-
-            dispatch(getUserDetails(id))
-
-        } else {
-
-            setName(user.name);
-
-            setEmail(user.email);
-
-            setRole(user.role)
-
-        }
-
-
-
-        if (error) {
-
-            errMsg(error);
-
-            dispatch(clearErrors());
-
-        }
-
-
-
-        if (isUpdated) {
-
-           successMsg('User updated successfully')
-
-            navigate('/admin/users')
-
-            dispatch({
-
-                type: UPDATE_USER_RESET
-
-            })
-
-        }
-
-    }, [dispatch, error, navigate, isUpdated, id, user])
-
-
-
-    const submitHandler = (e) => {
-
-        e.preventDefault();
-
-        const formData = new FormData();
-
-        formData.set('name', name);
-
-        formData.set('email', email);
-
-        formData.set('role', role);
-
-        dispatch(updateUser(user._id, formData))
-
+  useEffect(() => {
+    if (user && user._id !== id) {
+      dispatch(getUserDetails(id));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+      setRole(user.role);
+      setCompany(user.company);
+      setEmployeeId(user.employee_id);
     }
 
-    return (
+    if (error) {
+      errMsg(error);
+      dispatch(clearErrors());
+    }
 
-        <Fragment>
+    if (isUpdated) {
+      successMsg("User updated successfully");
+      navigate("/admin/users");
+      dispatch({
+        type: UPDATE_USER_RESET,
+      });
+    }
+  }, [dispatch, error, navigate, isUpdated, id, user]);
 
-            <MetaData title={`Update User`} />
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.set("name", name);
+    formData.set("email", email);
+    formData.set("role", role);
+    formData.set("company", company);
+    formData.set("employee_id", employeeId);
+    dispatch(updateUser(user._id, formData));
+  };
 
-            <div className="row">
+  return (
+    <Fragment>
+      <MetaData title={`Update User`} />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={2}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={12} md={10}>
+          <Grid container justifyContent="center">
+            <Grid item xs={10} lg={5}>
+              <Paper elevation={3} className="p-4" sx={{ marginTop: "20px" }}>
+                <form onSubmit={submitHandler}>
+                  <h1 className="mt-2 mb-5">Update User</h1>
+                  <TextField
+                    fullWidth
+                    label="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    sx={{ marginTop: "20px" }}
+                  />
+                  <TextField
+                    fullWidth
+                    label="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    sx={{ marginTop: "20px" }}
+                  />
+                  <FormControl fullWidth sx={{ marginTop: "20px" }}>
+                    <InputLabel>Role</InputLabel>
+                    <Select
+                      value={role}
+                      onChange={(e) => setRole(e.target.value)}
+                    >
+                      <MenuItem value="user">user</MenuItem>
+                      <MenuItem value="admin">admin</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <FormControl fullWidth sx={{ marginTop: "20px" }}>
+                    <InputLabel>
+                      Company
+                    </InputLabel>
+                    <Select
+                      value={company}
+                      onChange={(e) => setCompany(e.target.value)}
+                    >
+                      <MenuItem value="">Select Company</MenuItem>
+                      <MenuItem value="Barcino">Barcino</MenuItem>
+                      <MenuItem value="Meat Depot">Meat Depot</MenuItem>
+                      <MenuItem value="Single Origin">Single Origin</MenuItem>
+                      <MenuItem value="Bluesmith">Bluesmith</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <TextField
+                    fullWidth
+                    label="Employee ID"
+                    value={employeeId}
+                    onChange={(e) => setEmployeeId(e.target.value)}
+                    sx={{ marginTop: "20px" }}
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    className="mt-4 mb-3"
+                  >
+                    Update
+                  </Button>
+                </form>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Fragment>
+  );
+};
 
-                <div className="col-12 col-md-2">
-
-                    <Sidebar />
-
-                </div>
-
-                <div className="col-12 col-md-10">
-
-                    <div className="row wrapper">
-
-                        <div className="col-10 col-lg-5">
-
-                            <form className="shadow-lg" onSubmit={submitHandler}>
-
-                                <h1 className="mt-2 mb-5">Update User</h1>
-
-                                <div className="form-group">
-
-                                    <label htmlFor="name_field">Name</label>
-
-                                    <input
-
-                                        type="name"
-
-                                        id="name_field"
-
-                                        className="form-control"
-
-                                        name='name'
-
-                                        value={name}
-
-                                        onChange={(e) => setName(e.target.value)}
-
-                                    />
-
-                                </div>
-
-                                <div className="form-group">
-
-                                    <label htmlFor="email_field">Email</label>
-
-                                    <input
-
-                                        type="email"
-
-                                        id="email_field"
-
-                                        className="form-control"
-
-                                        name='email'
-
-                                        value={email}
-
-                                        onChange={(e) => setEmail(e.target.value)}
-
-                                    />
-
-                                </div>
-
-                                <div className="form-group">
-
-                                    <label htmlFor="role_field">Role</label>
-
-                                    <select
-
-                                        id="role_field"
-
-                                        className="form-control"
-
-                                        name='role'
-
-                                        value={role}
-
-                                        onChange={(e) => setRole(e.target.value)}
-
-                                    >
-
-                                        <option value="user">user</option>
-
-                                        <option value="admin">admin</option>
-
-                                    </select>
-
-                                </div>
-
-                                <button type="submit" className="btn update-btn btn-block mt-4 mb-3" >Update</button>
-
-                            </form>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </Fragment>
-
-    )
-
-}
-
-
-
-export default UpdateUser
-
+export default UpdateUser;
