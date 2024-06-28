3290a50786e33e5f04023ae2af1ba59e0875915d
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-06-28 14:05:57 +0800

    Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>Footer</div>
+    <div
+      style={{ display: "flex", justifyContent: "center", fontWeight: "bold" }}
+    >
+      RestaurantConcepts Group Inc. - 2024
+    </div>
   );
 };
 
